efc3f512a05f82e13879d447587bf94c34be0fbd
Andrea Bonilla <bc.andreac@gmail.com>
2025-02-07 19:01:06 -0500

    Refactor App component, update Categories page with new product categories, and adjust CSS for improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AppRouter from './routes/AppRouter'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Young Essence</h1>
       <AppRouter />
     </>
   )
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,48 +1,78 @@
 import React from "react";
 
-const products = [
+const categorias = [
   {
-    id: 1,
-    name: "Maquillaje",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
+      id: 1,
+      name: "Ropa",
+      imageSrc: "/src/assets/ropa.jpg",
+      imageAlt: "Model wearing light green crewneck t-shirt and light wash denim shorts standing on front porch of house.",
+      href: "#"
   },
   {
-    id: 2,
-    name: "Skin Care",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive oil pouring from spout of bottle into a bowl of olives and herbs.",
+      id: 2,
+      name: "Calzado",
+      imageSrc: "/src/assets/calzado.jpg",
+      imageAlt: "Model wearing black melange crewneck t-shirt, black melange joggers, and white sneakers.",
+      href: "#"
   },
-
-  // More products...
+  {
+      id: 3,
+      name: "Accesorios",
+      imageSrc: "/src/assets/accesorios.jpg",
+      imageAlt: "Model wearing black baseball cap, black long-sleeve crewneck t-shirt, black canvas messenger bag, and black denim pants.",
+      href: "#"
+  },
+  {
+      id: 4,
+      name: "Belleza y Cuidado Personal",
+      imageSrc: "/src/assets/belleza.jpg",
+      imageAlt: "Model wearing white crewneck t-shirt, black running shorts, and white sneakers.",
+      href: "#"
+  },
+  {
+    id: 5,
+    name: "Decoración",
+    imageSrc: "/src/assets/decoracion.jpg",
+    imageAlt: "Model wearing black baseball cap, black long-sleeve crewneck t-shirt, black canvas messenger bag, and black denim pants.",
+    href: "#"
+},
+{
+  id: 6,
+  name: "Peluches",
+  imageSrc: "/src/assets/peluches.jpg",
+  imageAlt: "Model wearing black baseball cap, black long-sleeve crewneck t-shirt, black canvas messenger bag, and black denim pants.",
+  href: "#"
+},
+{
+  id: 7,
+  name: "Tendencias",
+  imageSrc: "/src/assets/tendencia.jpg",
+  imageAlt: "Model wearing black baseball cap, black long-sleeve crewneck t-shirt, black canvas messenger bag, and black denim pants.",
+  href: "#"
+},
+{
+  id: 8,
+  name: "Todo",
+  imageSrc: "/src/assets/todo.jpg",
+  imageAlt: "Model wearing black baseball cap, black long-sleeve crewneck t-shirt, black canvas messenger bag, and black denim pants.",
+  href: "#"
+},
 ];
 
 export default function Categories() {
   return (
     <div className="bg-white">
+      <h1 className="text-black">Categorias</h1>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {categorias.map((product) => (
             <a key={product.id} href={product.href} className="group">
               <img
                 alt={product.imageAlt}
                 src={product.imageSrc}
                 className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
               />
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
+              <h3 className="mt-4 text-lg text-gray-800">{product.name}</h3>
             </a>
           ))}
         </div>
