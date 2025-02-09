cd99080087539f0cdc82b8f8497a9a7b2291c049
Andrea Bonilla <bc.andreac@gmail.com>
2025-02-09 12:32:37 -0500

    Remove unused image files, update CSS styles, add Home and Footer components, and enhance Navbar functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,18 @@ const Footer = () => {
     return (
         <footer className="bg-purple-400 text-white p-6 mt-10">
             <div className="container mx-auto flex flex-col items-center">
-                <h2 className="text-3xl font-bold mb-4 text-pink-200">Young</h2>
+                <h2 className="text-3xl font-bold mb-4 text-pink-200">Young Essence</h2>
                 <p className="text-center mb-4 text-yellow-100">
                     Creatividad, individualidad y modernidad en un solo lugar.
                 </p>
                 <div className="flex space-x-6 mb-4">
-                    <a href="https://www.instagram.com" className="text-pink-200 hover:text-pink-400">
+                    <a href="https://www.instagram.com" className="text-pink-200 hover:text-pink-800">
                         Instagram
                     </a>
-                    <a href="https://www.facebook.com" className="text-pink-200 hover:text-pink-400">
+                    <a href="https://www.facebook.com" className="text-pink-200 hover:text-pink-800">
                         Facebook
                     </a>
-                    <a href="https://www.twitter.com" className="text-pink-200 hover:text-pink-400">
+                    <a href="https://www.twitter.com" className="text-pink-200 hover:text-pink-800">
                         Twitter
                     </a>
                 </div>
